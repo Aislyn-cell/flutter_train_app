4e1ad65b4a74a989acf3fe9b756f605c0e252d9e
Aislyn-cell <lerrynjeong@gmail.com>
2025-03-25 09:14:39 +0900

    파일을 업데이트 했습니다.

--- lib/seat_page.dart
+++ lib/seat_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SeatPage extends StatelessWidget {
+  const SeatPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
